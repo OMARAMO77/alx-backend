62af4b80411fd220e60d581228b3a1ea4ffb50d0
Omar <omar0642624692@gmail.com>
2024-04-03 16:07:01 -0700

    Add 2-lifo_cache.py

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""LIFO caching module.
+"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """inherits from BaseCaching and is a caching
+    system with a LIFO removal mechanism
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            last_key = list(self.cache_data.keys())[-1]
+            print("DISCARD:", last_key)
+            del self.cache_data[last_key]
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Return value linked to key.
+        """
+        return self.cache_data.get(key, None)
